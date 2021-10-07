9d7b3e7d3c29595c10a5e03c524648e0e971c0b1
Stéphanie Corrias <scminos@gmail.com>
2021-10-07 20:09:58 +0200

    page product

--- front/js/product.js
+++ front/js/product.js
@@ -1,5 +1,22 @@
-class Products{
-    constructor(jsonProducts){
-        jsonProducts && Object.assign(this, jsonProducts);
-    }
-}
+const urlParams = new URLSearchParams(location.search).get('id')
+
+fetch(`http://localhost:3000/api/products/${urlParams}`)   
+    .then( (response) => {
+     return response.json()
+    })
+    .then((product) => {
+        console.log(product)
+
+        let imgProduct = document.createElement('img')
+        imgProduct.src = `${product.imageUrl}`
+        imgProduct.alt = `${product.altTxt}` 
+        console.log(imgProduct)
+        
+        
+    
+       
+
+    })
+    .catch((erreur) =>{
+        console.log(erreur)
+    })
