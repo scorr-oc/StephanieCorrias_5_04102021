b74fc8784ffde5949bcf2f04123080352fc65729
Stéphanie Corrias <scminos@gmail.com>
2021-10-10 17:13:04 +0200

    page produit

--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,7 @@
+// récupération de l'id du produit à afficher
+
 const urlParams = new URLSearchParams(location.search).get('id')
+
 
 fetch(`http://localhost:3000/api/products/${urlParams}`)   
     .then( (response) => {
@@ -7,16 +10,33 @@ fetch(`http://localhost:3000/api/products/${urlParams}`)
     .then((product) => {
         console.log(product)
 
+        // insertion de la photo du produit
         let imgProduct = document.createElement('img')
         imgProduct.src = `${product.imageUrl}`
         imgProduct.alt = `${product.altTxt}` 
-        console.log(imgProduct)
-        
-        
-    
-       
+        document.querySelector('.item__img').appendChild(imgProduct) 
 
+        // insertion du nom du produit
+        document.getElementById('title').innerHTML = `${product.name}`
+
+        // insertion du prix
+        document.getElementById('price').innerText = `${product.price} `
+
+        // insertion de la description
+        document.getElementById('description').innerText = `${product.description} `
+
+        // insertion des données du menu déroulant
+        let colors = product.colors 
+        console.log(colors)
+        colors.forEach((color) => {
+            let colorProduct = color
+            let colorMenu = document.createElement('option')
+            colorMenu.value = colorProduct
+            colorMenu.innerText = colorProduct
+            document.getElementById('colors').appendChild(colorMenu)
+            console.log(colorMenu)    
+        })
     })
-    .catch((erreur) =>{
+    .catch(erreur =>{
         console.log(erreur)
     })
