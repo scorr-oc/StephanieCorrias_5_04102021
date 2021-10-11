4d9ef31246cd590ecb5cdfc13fb9cb171242a4c0
Stéphanie Corrias <scminos@gmail.com>
2021-10-11 19:39:04 +0200

    panier 1

--- front/js/product.js
+++ front/js/product.js
@@ -2,13 +2,13 @@
 
 const urlParams = new URLSearchParams(location.search).get('id')
 
+// ----- Insertion du produit et de ses détails dans la page produit----
 
 fetch(`http://localhost:3000/api/products/${urlParams}`)   
     .then( (response) => {
      return response.json()
     })
     .then((product) => {
-        console.log(product)
 
         // insertion de la photo du produit
         let imgProduct = document.createElement('img')
@@ -27,15 +27,32 @@ fetch(`http://localhost:3000/api/products/${urlParams}`)
 
         // insertion des données du menu déroulant
         let colors = product.colors 
-        console.log(colors)
         colors.forEach((color) => {
             let colorProduct = color
             let colorMenu = document.createElement('option')
             colorMenu.value = colorProduct
             colorMenu.innerText = colorProduct
-            document.getElementById('colors').appendChild(colorMenu)
-            console.log(colorMenu)    
+            document.getElementById('colors').appendChild(colorMenu)  
         })
+
+        // --------Ajoût des produits dans le panier----------
+
+        // récuperation des valeurs du panier
+        var colorProduct = document.getElementById('colors').value
+        console.log(colorProduct)
+        var quantProduct = document.getElementById('quantity').value
+        console.log(quantProduct)
+        // création du panier
+        var cart = [urlParams,colorProduct,quantProduct]
+        console.log(cart)
+
+        // ajoût du produit dans le panier au clic
+        let button = document.getElementById('addToCart')
+
+        button.addEventListener('click',function(){
+
+        })
+
     })
     .catch(erreur =>{
         console.log(erreur)
